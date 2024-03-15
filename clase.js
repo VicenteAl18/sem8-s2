45b51e5148696cf385cf1c94eddd96209763236c
Italo Vicente <italo.aulavirtual@gmail.com>
2024-03-14 23:30:41 -0500

    Trabajo

---
+++ clase.js
@@ -0,0 +1,35 @@
+/*VARIABLES*/
+
+var lastName = 'VICENTE';
+lastName = 'ALEJANDRO';
+console.log(lastName);  /*Ccambia resultado*/
+
+let otherLastName = 'VILCHEZ';
+otherLastName = 'LUCANA';
+console.log(otherLastName);
+
+const lastYear = 2024;
+console.log(lastYear);
+
+/*STRING*/
+
+const fullName = '         Italo Vicente         ';
+
+console.log(fullName.trim().length);
+console.log(fullName.trim().toUpperCase());
+console.log(fullName.trim().toLowerCase());
+
+/*NUMBERS*/
+
+const numberOne = 25;
+const numberTwo = 32;
+const numberThree = 15.5;
+const numberFour = -210.35;
+
+let result;
+
+result = numberOne + numberTwo; /* adicion*/
+result = numberOne - numberThree; /* resta */
+result = numberOne * numberThree; /*producto */
+result = numberTwo / numberOne; /*division */
+result = numberTwo % numberOne; /* residuo */
---
+++ readme.md
@@ -0,0 +1,17 @@
+## VARIABLES ##
+
+1. TIPOS DE VARIABLES:
+
+    * var (ES5) : Nos permite definir variables al alcanse de su función mas sercana, osea dentro de la función
+    * let (ES6) : Nos permite definir variables que si se pueden cambiar.
+    * const (ES6): Nos permite declarar variables inmutables, osea no se puede cambiar.
+
+2. STRING
+
+    * Permite ecaracteres de texto
+
+3. NUMBERS
+
+    *  Se expresa numeros enteros con int, decimales double, nuneros random  y para hacer operaciones aritmeticas.
+
+    
